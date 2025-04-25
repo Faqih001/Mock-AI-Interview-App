47f1e3438cf6a586afa55a77c70ef721181d3109
Amirul Faqih <fakiiahmad001@gmail.com>
2025-04-25 22:00:16 +0300

    fix: update hasUpcomingInterviews logic to handle undefined allInterview length

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -19,7 +19,7 @@ async function Home() {
   ]);
 
   const hasPastInterviews = (userInterviews?.length ?? 0) > 0;
-  const hasUpcomingInterviews = allInterview?.length! > 0;
+  const hasUpcomingInterviews = (allInterview?.length ?? 0) > 0;
 
   return (
     <>
