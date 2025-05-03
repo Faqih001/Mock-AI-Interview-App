35b6e80266c87c9158227d86263abdb4ba504ee3
Amirul Faqih <fakiiahmad001@gmail.com>
2025-05-03 23:55:21 +0300

    fix: add comment to clarify purpose of getTechLogos function

--- lib/utils.ts
+++ lib/utils.ts
@@ -26,6 +26,7 @@ const checkIconExists = async (url: string) => {
   }
 };
 
+// getTechLogos is a function that takes an array of tech names as input
 export const getTechLogos = async (techArray: string[]) => {
   const logoURLs = techArray.map((tech) => {
     const normalized = normalizeTechName(tech);
